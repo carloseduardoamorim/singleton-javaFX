adac532c850012e30ae4d72d87d3b27ba63854e9
carloseduardoamorim <caduamorim13@gmail.com>
2021-12-01 11:52:15 -0300

    Singleton com JavaFX/OpenJFX

---
+++ src/main/java/com/mycompany/myapp/model/Config.java
@@ -0,0 +1,49 @@
+package com.mycompany.myapp.model;
+
+/**
+ *
+ * @author cadua
+ */
+public class Config {
+    private static final Config instance = new Config();
+    
+    private String formatRelatorios = "html";
+    private String tipoGraficos = "pizza";
+
+    private Config() {
+    }
+    
+    public static Config getIntance(){
+        return instance;
+    }
+    
+    /**
+     * @return the formatRelatorios
+     */
+    public String getFormatRelatorios() {
+        return formatRelatorios;
+    }
+
+    /**
+     * @param formatRelatorios the formatRelatorios to set
+     */
+    public void setFormatRelatorios(String formatRelatorios) {
+        this.formatRelatorios = formatRelatorios;
+    }
+
+    /**
+     * @return the tipoGraficos
+     */
+    public String getTipoGraficos() {
+        return tipoGraficos;
+    }
+
+    /**
+     * @param tipoGraficos the tipoGraficos to set
+     */
+    public void setTipoGraficos(String tipoGraficos) {
+        this.tipoGraficos = tipoGraficos;
+    }
+    
+    
+}
